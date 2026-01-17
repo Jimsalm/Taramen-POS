d54f4352c614e8b4753ee1b46abcbf054b17f676
Jimsalm <hypermood15.jimiel@gmail.com>
2026-01-18 02:54:47 +0800

    BACKEND
    ADD
    Employee controller
    [
    Get all active employee,
    Get all employee,
    Create new employee,
    Get employee by id,
    Update employee by id,
    Toggle status of employee,
    Archive employee.

--- taramen-pos-backend/routes/api.php
+++ taramen-pos-backend/routes/api.php
@@ -13,4 +13,5 @@ Route::prefix('v1')->group(function () {
     require __DIR__ . '/api/v1/auth.php';
     require __DIR__ . '/api/v1/category.php';
     require __DIR__ . '/api/v1/menu_item.php';
+    require __DIR__ . '/api/v1/employee.php';
 });
