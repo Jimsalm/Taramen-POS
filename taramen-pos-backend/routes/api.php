e702f2c525c790e073eeebee23cde5f0ad76d922
Juan Rafael Camasis <sonic92yeylol@gmail.com>
2026-01-24 18:28:28 +0800

    feat: updated the api.php
    This is to add the report route to the api

--- taramen-pos-backend/routes/api.php
+++ taramen-pos-backend/routes/api.php
@@ -16,4 +16,5 @@ Route::prefix('v1')->group(function () {
     require __DIR__ . '/api/v1/employee.php';
     require __DIR__ . '/api/v1/discount.php';
     require __DIR__ . '/api/v1/order.php';
+    require __DIR__ . '/api/v1/report.php';
 });
