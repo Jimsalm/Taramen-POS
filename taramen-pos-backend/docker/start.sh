fe7778219a66e33e4e080867e1097be527751a6b
Jimiel Salmon <hypermood15.jimiel@gmail.com>
2026-02-05 15:07:28 +0800

    seeder

--- taramen-pos-backend/docker/start.sh
+++ taramen-pos-backend/docker/start.sh
@@ -2,21 +2,19 @@
 set -e
 
 : "${PORT:=10000}"
-
-# generate nginx.conf from template
 sed "s/__PORT__/${PORT}/g" /var/www/html/docker/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Make sure Laravel sees Render env vars
 php artisan config:clear
-
-# Optional but good for performance
 php artisan config:cache
 
-echo "DB_HOST=$DB_HOST"
-echo "DB_DATABASE=$DB_DATABASE"
-echo "DATABASE_URL=$DATABASE_URL"
-
-# Run migrations once
 php artisan migrate --force
+
+# Seed only if empty
+php -r "
+require 'vendor/autoload.php';
+\$app = require 'bootstrap/app.php';
+\$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+exit(\App\Models\User::query()->exists() ? 0 : 1);
+" || php artisan db:seed --force
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
