cb480963c49c14de782bc3e371beae4d2d3429cd
Jimiel Salmon <hypermood15.jimiel@gmail.com>
2026-02-05 13:08:58 +0800

    testing docker

--- taramen-pos-backend/config/database.php
+++ taramen-pos-backend/config/database.php
@@ -92,10 +92,10 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => '', 
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [
