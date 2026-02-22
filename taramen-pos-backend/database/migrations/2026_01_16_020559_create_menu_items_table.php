24201c722bcd8ebe20cacf1b567d9e06a0b4f268
Juan Rafael Camasis <sonic92yeylol@gmail.com>
2026-02-22 16:29:55 +0800

    feat: adjusted image creation
     this is to have separate table for the image

--- taramen-pos-backend/database/migrations/2026_01_16_020559_create_menu_items_table.php
+++ taramen-pos-backend/database/migrations/2026_01_16_020559_create_menu_items_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
             $table->boolean('status')->default(true);
             $table->boolean('available')->default(true);
-            $table->string('image')->nullable();
+            $table->foreignId('image_id')->nullable()->constrained("files");
             $table->timestamps();
             $table->softDeletes();
         });
